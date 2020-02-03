00cca4b93f5d39eca5656c2e516ad5e1d8b2fbfe
Moon1ord <e.shmarev@frowardmg.ru>
2020-02-03 18:33:43 +0300

    added pagination right

--- gallery.js
+++ gallery.js
@@ -1,30 +1,34 @@
 class Galereya{
-    constructor(class_selector){
+    constructor(class_selector, params){
         const root_class = 'glry';
         this.elements = document.querySelectorAll("."+class_selector);
         this.sliders = [];
         class Slider{
-            constructor(wrapper, root_class){
+            constructor(wrapper, root_class, params){
                 const self = this;
                 this.slides = wrapper.querySelectorAll('div');
                 this.amountOfSlides = this.slides.length;
                 this.currentSlide = 0;
+                this.moveToRightSlide = ()=>{paginationRight();};
+                this.moveToLeftSlide = ()=>{paginationLeft();};
 
-                wrapper.addEventListener('click', function(){
+                function paginationRight(){
+                    console.log(self.currentSlide);
+
                     if(self.currentSlide < self.amountOfSlides - 1){
                         self.currentSlide++;
                     }else{
-                        self.currentSlide = 0;
+                        self.currentSlide = 0
                     }
-
                     wrapper.style.transform = 'translateX(' + self.currentSlide * -100 + '%)'; 
-                });
+                    console.log(self.currentSlide);
+                };
 
-                this.slides.forEach(function(slide, index){
+                params.right_arrow.addEventListener('click', this.moveToRightSlide);
+                params.left_arrow.addEventListener('click', this.moveToLeftSlide);
+                
+                this.slides.forEach(function(slide){
                     slide.classList.add(root_class + '-slide')
-                    if(index === 0){
-                        slide.classList.add('current');
-                    }
                 });
             }
         }
@@ -33,7 +37,7 @@ class Galereya{
             element.classList.add(root_class);
             let wrapper = element.children[0];
             wrapper.classList.add(root_class + '-wrapper');
-            let slider = new Slider(wrapper, root_class);
+            let slider = new Slider(wrapper, root_class ,params);
             this.sliders.push(slider);
         });
     }
